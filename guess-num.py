f130bf19643192deefc345cb06a2f4f7e4ede0fb
ms197973 <ms197973@gmail.com>
2024-08-20 05:34:27 +0800

    Add files via upload

--- guess-num.py
+++ guess-num.py
@@ -1,8 +1,13 @@
 #047 猜數字
 
 import random
+start = input('請輸入隨機數字範圍起始值：')
+end = input('請輸入隨機數字範圍結束值')
+start = int(start)
+end = int(end)
+
 t=0
-r = random.randint(1, 100)
+r = random.randint(start, end)
 while True:
 	t += 1
 	num = input('請猜數字：')
@@ -13,8 +18,7 @@ while True:
 		break
 	elif num > r:
 		print('比答案大')
-		print('猜了第',t,'次')
 	elif num < r:
 		print('比答案小')
-		print('猜了第',t,'次')
+	print('猜了第',t,'次')
 
